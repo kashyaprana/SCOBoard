2a27c1c76693daf5f8d434bd9ad65db859e68dec
aymen0627 <77123184+aymen0627@users.noreply.github.com>
2022-11-14 21:21:25 -0500

    Overhaul of project
    Includes new authentication feature with working login. Current system does NOT have create account functionality, it's manually created using firebase

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAwH4sMQXZwc6YnCEqoLuD0Hlk71dh46Ew",
+  authDomain: "scoapp-8527b.firebaseapp.com",
+  databaseURL: "https://scoapp-8527b-default-rtdb.firebaseio.com",
+  projectId: "scoapp-8527b",
+  storageBucket: "scoapp-8527b.appspot.com",
+  messagingSenderId: "964766561619",
+  appId: "1:964766561619:web:3237e67c6efd3649d7dda6",
+  measurementId: "G-B6EJ2L4YZ5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+
+export default app
