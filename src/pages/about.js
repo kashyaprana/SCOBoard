69ed0a6f2e7747d13ce5652bcde9dbeafcf4e6ad
aymen0627 <77123184+aymen0627@users.noreply.github.com>
2022-11-15 21:52:10 -0500

    Delete src directory

--- src/pages/about.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-
-export const AboutPage = () => {
-    return (
-        <h1> AboutPage </h1>
-    );
-}
--- src/pages/scheduler.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-
-export const SchedulerPage = () => {
-    return (
-        <h1> Schedule Page </h1>
-    );
-}
