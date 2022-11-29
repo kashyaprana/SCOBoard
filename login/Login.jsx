85927534fecd3a474a472c974c599a914f11c6a8
aymen0627 <77123184+aymen0627@users.noreply.github.com>
2022-11-28 21:50:27 -0500

    center login & register pages
    Centered boxes to register and login; centered in front of video

---
+++ login/Login.jsx
@@ -0,0 +1,71 @@
+import "./login.scss"
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import { AuthProvider, useAuth } from "../../contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom"
+import videoUCF from "../../images/chargeon.mp4"
+
+
+
+
+
+export default function Login(){
+
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    
+    const { login, currentUser,} = useAuth()
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+   
+    const navigate = useNavigate()
+    
+
+      async function handleLogin(e){
+      e.preventDefault()
+      
+      try {
+        setError("")
+        setLoading(true)
+        await login(emailRef.current.value, passwordRef.current.value) 
+
+        navigate('/admin')     
+      } catch {
+        setError("Failed to log in")
+      }
+  
+      setLoading(false)
+    }
+
+
+    return (
+
+      
+        <div className="login">
+          
+          <video src = {videoUCF} autoPlay loop muted />
+            
+          <div className = "overlay">
+        <form onSubmit = {handleLogin}>
+          <h1> Welcome to the UCF Student Conduct Office</h1>
+          {error && <Alert variant="danger">{error}</Alert>}
+          
+            <input type = "email" placeholder = "Email" ref = {emailRef}/>
+            <input type = "password" placeholder = "Password" ref = {passwordRef} />
+            <button disabled = {loading}  type = "submit"> Login</button> 
+            
+            <div>
+         <Link to="/">Go Back</Link>
+         
+          </div>
+          
+        </form>
+        </div>
+        </div>
+       
+      
+        
+    )
+    }
+
+
