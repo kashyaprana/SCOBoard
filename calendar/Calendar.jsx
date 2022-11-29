912c7995951303e587e2d559d3f5d4676516cb9f
aymen0627 <77123184+aymen0627@users.noreply.github.com>
2022-11-28 22:55:39 -0500

    added image to calendar page
    Re-added pegasus image to background of calendar page, adjusted margins of calendar too

---
+++ calendar/Calendar.jsx
@@ -0,0 +1,31 @@
+import "./calendar.scss"
+import React from 'react'
+
+import {useNavigate} from "react-router-dom";
+
+import pegasus from "../../images/pegasus.png"
+import Nav from "../../components/adminNav/Nav.jsx"
+
+
+
+
+const Calendar = () => {
+    const navigate = useNavigate() //used for button redirects
+    return (
+        <div className = "navCal">
+        < Nav />
+        <div className="calendar">
+            <div className = "pegasus-pic">
+            <img src={pegasus} alt="pegasus" className="ucf" />
+            
+            </div>
+            <div className = "fixed">
+            <iframe title = "caly" src="https://calendar.google.com/calendar/embed?height=800&wkst=1&bgcolor=%23F6BF26&ctz=America%2FNew_York&showTitle=0&src=YXltZW5oMDYyN0BnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=OHF0ZzI1M2J0Yms3bW1yZWdwdG52M2VlZWVzdWlrbmtAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%23F6BF26&color=%230B8043" width="1300" height="900" frameborder="0" scrolling="no"></iframe>
+            <button onClick = {() => navigate('/admin')} >Back to Home</button>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Calendar
